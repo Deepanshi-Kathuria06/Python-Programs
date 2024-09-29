67c0cb4a1538bd3bb3750ea2a9bba9d629d1957d
Deepanshi <kathuriadeepanshi56@gmail.com>
2024-09-29 11:24:44 +0530

    Variables

--- variable.py
+++ variable.py
@@ -1,8 +1,11 @@
 #  a variable name is given to the memory location in a program
 
 name = "Deepanshi"   #string in double quotes
-marks =  12
-age = 13
-print(name)
+marks =  12 #int
+age = 13.5 #float
+print("my name is:" , name)
 print(age)
 print(marks)
+
+print(type(name))
+print(type(age))
